e223af38590d181df8e6b60ebf5037e2a78b12b8
Kenji Inoue <kenji.inoue@hotmail.com>
2023-10-02 16:55:43 -0300

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <footer>
+        <p>
+          <span>React + TS To-do</span> @ 2021
+        </p>
+      </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from "./Header.module.css"
+
+const Header = () => {
+    return (
+        <header>
+        <h1>React + TS To-do List</h1>
+      </header>
+    )
+}
+
+export default Header
